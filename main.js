0eff1aa26e81b2363e3c123647901dc88298aa4a
Adám Brudzewsky <a.brudz@gmail.com>
2022-09-21 09:54:05 +0100

    strip for aplan

--- main.js
+++ main.js
@@ -3,9 +3,6 @@ w = window; params = new URLSearchParams(w.location.search);
 run = async (id) => {
     const i = document.getElementById(id);
   
-    const infx = document.getElementById(`${id}Y`)
-    const prfx = document.getElementById(`X${id}Y`)
-  
     let socket = new WebSocket("wss://dyalog.run/api/v0/ws/execute");
   
     socket.onopen = () => {
@@ -14,12 +11,17 @@ run = async (id) => {
       MessagePack.encode({
         language: "dyalog_apl",
         code:
-        "⎕FIX':Class class' '∇ kill' ':Implements Destructor' '⎕ ← ⎕json 2⍴⊂,'' ''' '∇' ':EndClass'\n_←⎕new class\n"  
-          + explicit
-          + "\n ⎕ ← ⎕JSON explicit '"
-          + expr.replace(/\'/g, "''")
-          + "'",
-        timeout: 5,
+   `⎕FIX'file://',(2⎕NQ#'GetEnvironment' 'DYALOG'),'/StartupSession/Dyalog/Array.apln'
+    'display'⎕CY'dfns'
+     Show←{
+         2=⎕NC'⍵':display ⍵
+         0::'[namespace]'
+         r←⍵.((⊂,⊂∘∇∘⍎)¨⎕NL-2 9)
+         r,←⍵.((⊂,⊂∘⎕CR)¨⎕NL-3 4)
+         ↑r[⍋r]
+     }
+     ⎕←1⎕JSON↓⎕FMT Show Array.Deserialise 0⎕JSON'${JSON.stringify(input.value).replace(/\'/g, "''")}'`,
+        timeout: 1,
       })
     );
   };
@@ -27,54 +29,18 @@ run = async (id) => {
     socket.onmessage = async (event) => {
       try { 
        const stream = await MessagePack.decodeAsync(event.data.stream())
-       values = JSON.parse(new TextDecoder().decode(stream.stdout).split("\n")[0]);
-       infx.textContent = values[0];
-       prfx.textContent = values[1];
+       output.value = JSON.parse(new TextDecoder().decode(stream.stdout)).join("\n");
       } catch {
-       infx.textContent = 'Internal Server Error';
-       prfx.textContent = 'Internal Server Error';
+       output.value = 'Internal Server Error';
      }
     };
   }
   
 Copy = () => {
-  w.history.replaceState({},w.document.title,w.location.pathname 
-    + ("?f=" + encodeURIComponent(document.getElementById("f").value))
-    + (document.getElementById('V2').style.display != 'none' ? ("&g=" + encodeURIComponent(document.getElementById("g").value)) : ""));
+  w.history.replaceState({},w.document.title,w.location.pathname + "?a=" + encodeURIComponent(input.value) );
   
   navigator.clipboard.writeText(w.location.toString().replace("#",""))
 }
 
-Split = (e) => {
-    const pane = document.getElementById("V2");
-    if (pane.style.display == "inherit") {
-      pane.style.display = "none";
-      e.textContent = "+";
-      document.getElementById("swap").style.display = "none";
-    } else {
-      pane.style.display = "inherit";
-      e.textContent = "-";
-      document.getElementById("swap").style.display = "inherit";
-    }
-  };
-
-Swap = () => {
-    let a,b,c,x,y,z;
-
-    [a,b,c] = [document.getElementById('f'), document.getElementById('fY'), document.getElementById('XfY')];
-    [x,y,z] = [document.getElementById('g'), document.getElementById('gY'), document.getElementById('XgY')];
-
-
-    [a.value,b.textContent,c.textContent,x.value,y.textContent,z.textContent] = [x.value,y.textContent,z.textContent,a.value,b.textContent,c.textContent];
-}
-
-if (params.get("f") != null) { document.getElementById("f").value = decodeURIComponent(params.get("f")); }
-if (params.get("g") != null) { 
- document.getElementById("g").value = decodeURIComponent(params.get("g"));
- document.getElementById("V2").style.display = 'inherit';
- document.getElementById("expand").textContent = '-'; 
- document.getElementById("swap").style.display = "inherit";
-}
-
-run('f')
-run('g')
+if (params.get("a") != null) { input.value = decodeURIComponent(params.get("a")); }
+run('input')
